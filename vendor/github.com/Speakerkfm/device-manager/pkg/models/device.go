590ac37659601c8df8789e7b205da7f779784a00
al.usanin <al.usanin@xsolla.com>
2020-01-17 11:00:23 +0500

    fix dep

--- vendor/github.com/Speakerkfm/device-manager/pkg/models/device.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Device struct {
-	ID   string `json:"id" jwt:"id"`
-	Name string `json:"name" jwt:"name"`
-
-	LastMeterReadingsTime *string `json:"last_meter_readings_time" jwt:"-"`
-}
--- vendor/github.com/Speakerkfm/device-manager/pkg/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID    string `json:"id" jwt:"id"`
-	Email string `json:"email" jwt:"email"`
-
-	DevicesIDs []string `json:"-" jwt:"-"`
-}
